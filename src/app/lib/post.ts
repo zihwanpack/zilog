9f64fc4225d16724a9c87f4b523ac993fc1ced22
Layton <jihwan8807@naver.com>
2026-03-30 14:07:35 +0900

    feature: 마크다운을 위한 tailwind typography 추가 적용, turbopack을 위한 root 폴더 위치 설정

--- src/app/lib/post.ts
+++ src/app/lib/post.ts
@@ -1,12 +1,12 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import matter from 'gray-matter';
+import fs from "node:fs";
+import path from "node:path";
+import matter from "gray-matter";
 
-const postsDirectory = path.join(process.cwd(), 'content/posts');
+const postsDirectory = path.join(process.cwd(), "src/app/content/posts");
 
 export function getPostBySlug(slug: string) {
   const fullPath = path.join(postsDirectory, `${slug}.mdx`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
   return { data, content };
