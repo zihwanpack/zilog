bdf62d5b9000b3429d9c7786a0c1bf43feb16e73
Layton <jihwan8807@naver.com>
2026-03-30 16:44:59 +0900

    feature: getPostBySlug 캐싱 설정 추가

--- src/app/lib/post.ts
+++ src/app/lib/post.ts
@@ -4,13 +4,14 @@ import matter from "gray-matter";
 
 const postsDirectory = path.join(process.cwd(), "src/app/content/posts");
 
-  export function getPostBySlug(slug: string) {
-    const fullPath = path.join(postsDirectory, `${slug}.mdx`);
-    if (!fs.existsSync(fullPath)) {
-      throw new Error(`포스트를 찾을 수 없습니다: ${slug}`);
-    }
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-    const { data, content } = matter(fileContents);
-  
+export function getPostBySlug(slug: string) {
+  "use cache";
+  const fullPath = path.join(postsDirectory, `${slug}.mdx`);
+  if (!fs.existsSync(fullPath)) {
+    throw new Error(`포스트를 찾을 수 없습니다: ${slug}`);
+  }
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
   return { data, content };
 }
