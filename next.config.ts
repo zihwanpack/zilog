ecf8aec34c948fa853fc896400629ac85d754beb
Layton <jihwan8807@naver.com>
2026-03-31 23:17:22 +0900

    feature: cache component 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 import path from "path";
 const nextConfig: NextConfig = {
-  /* config options here */
   turbopack: {
     root: path.join(__dirname),
   },
   reactCompiler: true,
+  cacheComponents: true,
 };
 
 export default nextConfig;
